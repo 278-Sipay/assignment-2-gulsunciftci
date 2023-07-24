a114e72fcec21afaaf886b4b98203e75651cc92a
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-07-24 03:15:57 +0300

    ikinci ödev

---
+++ Sipay-Bootcamp-Hafta-2/SipayApi.DataAccess/Repository/TransactionRepository/TransactionRepository.cs
@@ -0,0 +1,15 @@
+﻿using SipayApi.DataAccess.ApplicationDbContext;
+using SipayApi.DataAccess.Domain;
+using SipayApi.DataAccess.Repository.Base;
+
+namespace SipayApi.DataAccess.Repository.TransactionRepository
+    {
+    public class TransactionRepository : GenericRepository<Transaction>, ITransactionRepository
+    {
+        public TransactionRepository(SimDbContext simDbContext) : base(simDbContext)
+        {
+        }
+    }
+}
+
+
---
+++ Sipay-Bootcamp-Hafta-2/SipayApi.Service/Controllers/TransactionController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SipayApi.Base.Response;
+using SipayApi.DataAccess.Domain;
+using SipayApi.DataAccess.Repository;
+using System.Transactions;
+using Transaction = SipayApi.DataAccess.Domain.Transaction;
+
+namespace SipayApi.Service.Controllers
+{
+    [ApiController]
+    [Route("sipy/api/[controller]")]
+    public class TransactionController : ControllerBase
+    {
+        private readonly ITransactionRepository repository;
+        public TransactionController(ITransactionRepository repository)
+        {
+            this.repository = repository;
+        }
+
+
+        [HttpGet]
+        public ApiResponse<List<Transaction>> GetAll()
+        {
+            var entityList = repository.GetAll();
+            return new ApiResponse<List<Transaction>>(entityList);
+        }
+
+        [HttpGet("{id}")]
+        public ApiResponse<Transaction> Get(int id)
+        {
+            var entity = repository.GetById(id);
+            return new ApiResponse<Transaction>(entity);
+        }
+
+
+        [HttpPost]
+        public ApiResponse Post([FromBody] Transaction request)
+        {
+            repository.Insert(request);
+            return new ApiResponse();
+        }
+
+        [HttpPut("{id}")]
+        public ApiResponse Put(int id, [FromBody] Transaction request)
+        {
+            repository.Update(request);
+            return new ApiResponse();
+        }
+
+
+        [HttpDelete("{id}")]
+        public ApiResponse Delete(int id)
+        {
+            repository.DeleteById(id);
+            return new ApiResponse();
+        }
+
+
+    }
+
+}
